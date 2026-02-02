666137237c5dc3e43f5665868583e06c05eef0eb
Kanbarakun <caguiclajordan1212@gmai.com>
2026-02-02 17:15:31 +0800

    fixed the name filheandler.js file name

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const fileHandler = require('../utils/filehandler');
+const fileHandler = require('../utils/filehandler.js');
 
 // Registration Endpoint
 router.post('/register', (req, res) => {
